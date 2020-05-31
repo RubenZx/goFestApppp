3d037413b508b3c0be02de952ac670f4df112920
RubenZx <ruben.mondom@gmail.com>
2020-05-31 17:56:08 +0200

    :sparkles: added google map on location vieew

--- app/src/main/java/es/uca/gofestapppp/ui/location/LocationFragment.java
+++ app/src/main/java/es/uca/gofestapppp/ui/location/LocationFragment.java
@@ -1,3 +1,4 @@
+
 package es.uca.gofestapppp.ui.location;
 
 import android.os.Bundle;
@@ -8,12 +9,92 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.jetbrains.annotations.NotNull;
+
 import es.uca.gofestapppp.R;
 
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
+    private static final String MAPVIEW_BUNDLE_KEY ="AIzaSyDL0hor45EroBCO-Hcb_q4ZU-pmv1ItIz8";
+    private MapView mMapView;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_location, container, false);
+        mMapView = root.findViewById(R.id.map);
+        initGoogleMap(savedInstanceState);
         return root;
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState){
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView.onCreate(mapViewBundle);
+        mMapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NotNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng latLng = new LatLng(40.423889,-3.671667);
+        map.addMarker(new MarkerOptions().position(latLng).title("Wizink Center"));
+        map.moveCamera( CameraUpdateFactory.newLatLngZoom(latLng , 14f) );
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
     }
 }
